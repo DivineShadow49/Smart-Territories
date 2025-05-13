4a67451617c8b1cd4c646ab9eb45aa98f5366fdf
Théo Bourgoin <117354336+DivineShadow49@users.noreply.github.com>
2025-05-13 15:11:38 +0200

    Ajout logs analyse.js

--- Réalisation_Projet/Code_ServeurTraitement/analyse.js
+++ Réalisation_Projet/Code_ServeurTraitement/analyse.js
@@ -17,7 +17,8 @@ class CAnalyse {
         // Vérifie la présence des champs obligatoires
         for (const champ of champsObligatoires) {
             if (!(champ in dataMQTT)) {
-                console.error(`Champ manquant : ${champ}`);
+                //console.error(`Champ manquant : ${champ}`);
+		logger.warn(`Champ manquant dans le paquet MQTT : ${champ}`);
                 return false;
             }
         }
@@ -25,13 +26,15 @@ class CAnalyse {
         // Vérifie le format de la date : "AAAA-MM-JJ HH"
 	const regexDate = new RegExp("^\\d{4}-\\d{2}-\\d{2} ((0\\d)|(1\\d)|(2[0-3]))$");
         if (!regexDate.test(dataMQTT.Date)) {
-            console.error("Format de date invalide. Format attendu : 'AAAA-MM-JJ HH'");
+            //console.error("Format de date invalide. Format attendu : 'AAAA-MM-JJ HH'");
+	    logger.warn("Format de date invalide reçu dans le paquet MQTT. Format attendu : 'AAAA/MM/JJ HH'");
             return false;
         }
 
         // Vérifie que la valeur est un nombre
         if (isNaN(parseFloat(dataMQTT.Valeur))) {
-            console.error("Valeur non numérique");
+            //console.error("Valeur non numérique");
+	    logger.warn("Champ 'Valeur' non numérique dans le paquet MQTT.");
             return false;
         }
 
@@ -43,9 +46,11 @@ class CAnalyse {
         if (this.verif_dataMQTT(dataMQTT)) {
             try {
                 const res = await axios.post(`${this.apiUrl}${this.cheminEnvoi}`, dataMQTT);
-                console.log("Donnée envoyée à l’API :", res.data);
+                //console.log("Donnée envoyée à l’API :", res.data);
+		logger.info(`Donnée MQTT envoyée à l’API avec succès : ${JSON.stringify(res.data)}`);
             } catch (error) {
-                console.error("Erreur lors de l’envoi à l’API :", error.message);
+                //console.error("Erreur lors de l’envoi à l’API :", error.message);
+		logger.warn(`Erreur lors de l’envoi du paquet MQTT à l’API : ${error.message
             }
 	}
     }
